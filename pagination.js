59f88968c49e8a899ce036e93a980b725a6cab0b
Caio Márcio <caio.marcio@vrglass.com>
2024-10-04 01:41:51 -0300

    Removing some models

--- pagination.js
+++ pagination.js
@@ -1,18 +1,13 @@
 const productImages = [
     { image: 'assets/images/poltrona-lacuna.png', name: 'Poltrona Lacuna', model: 'assets/models/poltrona_lacuna.glb' },
     { image: 'assets/images/poltrona-pollux.png', name: 'Poltrona Pollux', model: 'assets/models/poltrona_pollux.glb' },
-    { image: 'assets/images/poltrona-pollux.png', name: 'Espelho Nexo', model: 'assets/models/espelho_nexo.glb' },
-    { image: 'assets/images/cadeira-trapezio.png', name: 'Cadeira Girafa', model: 'assets/models/cadeira_girafa.glb' },
     { image: 'assets/images/poltrona-lacuna.png', name: 'Poltrona Bauhaus', model: 'assets/models/poltrona_bauhaus.glb' },
-    { image: 'assets/images/cadeira-trapezio.png', name: 'Cadeira Trapézio', model: 'assets/models/cadeira_trapezio.glb' },
-    { image: 'assets/images/cadeira-trapezio.png', name: 'Buffet Aero', model: 'assets/models/buffet_aero.glb' },
     { image: 'assets/images/cadeira-trapezio.png', name: 'Cadeira Sushi', model: 'assets/models/cadeira_sushi.glb' },
     { image: 'assets/images/cadeira-trapezio.png', name: 'Cadeira Madeleine', model: 'assets/models/cadeira_madeleine.glb' },
     { image: 'assets/images/cadeira-trapezio.png', name: 'Poltrona Cuore', model: 'assets/models/poltrona_cuore.glb' },
     { image: 'assets/images/cadeira-trapezio.png', name: 'Cadeira Garça', model: 'assets/models/cadeira_garca.glb' },
     { image: 'assets/images/cadeira-trapezio.png', name: 'Poltrona Itiquira', model: 'assets/models/poltrona_itiquira.glb' },
-    { image: 'assets/images/cadeira-trapezio.png', name: 'Cadeira Girafa Bo Bardi', model: 'assets/models/cadeira_girafa_bo_bardi.glb' },
-    { image: 'assets/images/poltrona-bauhaus.png', name: 'Poltrona Bowl', model: 'assets/models/poltrona_bowl.glb' }
+    { image: 'assets/images/cadeira-trapezio.png', name: 'Cadeira Girafa', model: 'assets/models/cadeira_girafa.glb' }
 ];
 
 let currentPage = 0;
